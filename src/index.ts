abddbe3c23770ce018466bcf7214248d2e17fdf5
Alex Ramirez <alexander.ramirez@gmail.com>
2024-01-05 04:07:56 -0500

    feat: Create IPFS web3 plugin

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,58 @@
 import { Web3PluginBase } from "web3";
+import type { ContractAbi } from "web3";
+import type { CID } from "ipfs-http-client";
+import { promises as fsPromises } from "fs";
+import { create } from "ipfs-http-client";
 
-export class TemplatePlugin extends Web3PluginBase {
-  public pluginNamespace = "template";
+// import { ABI } from "./abi";
+
+export class IpfsPlugin extends Web3PluginBase {
+  public pluginNamespace: string;
+  private ipfsClient;
+
+  public constructor(
+    options: {
+      pluginNamespace?: string;
+      contractAbi?: ContractAbi;
+      ipfsApiUrl?: string;
+    } = {},
+  ) {
+    super();
+    this.pluginNamespace = options.pluginNamespace ?? "ipfs";
+    this.ipfsClient = create({
+      url: options.ipfsApiUrl || "http://localhost:5001",
+    });
+  }
 
   public test(param: string): void {
     console.log(param);
+  }
+
+  public async uploadFile(
+    path: string,
+  ) {
+    const buffer = await fsPromises.readFile(path);
+    console.log(`Uploading file ${path} to IPFS...`);
+    const addedFile = await this.ipfsClient.add(buffer);
+    console.log("Added file CID:", addedFile.cid.toString());
+
+    return await this.storeCID(addedFile.cid);
+  }
+
+  private async storeCID(cid: CID) {
+    console.log(`Storing CID ${cid} on Ethereum...`);
+  }
+
+  public async listCIDs(
+    ethereumAddress: string,
+  ) {
+    console.log(`Listing CIDs for address ${ethereumAddress}...`);
   }
 }
 
 // Module Augmentation
 declare module "web3" {
   interface Web3Context {
-    template: TemplatePlugin;
+    ipfs: IpfsPlugin;
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,21 +1,21 @@
 import { Web3, core } from "web3";
-import { TemplatePlugin } from "../src";
+import { IpfsPlugin } from "../src";
 
-describe("TemplatePlugin Tests", () => {
-  it("should register TemplatePlugin plugin on Web3Context instance", () => {
+describe("IpfsPlugin Tests", () => {
+  it("should register IpfsPlugin plugin on Web3Context instance", () => {
     const web3Context = new core.Web3Context("http://127.0.0.1:8545");
-    web3Context.registerPlugin(new TemplatePlugin());
-    expect(web3Context.template).toBeDefined();
+    web3Context.registerPlugin(new IpfsPlugin());
+    expect(web3Context.ipfs).toBeDefined();
   });
 
-  describe("TemplatePlugin method tests", () => {
+  describe("IpfsPlugin method tests", () => {
     let consoleSpy: jest.SpiedFunction<typeof global.console.log>;
 
     let web3: Web3;
 
     beforeAll(() => {
       web3 = new Web3("http://127.0.0.1:8545");
-      web3.registerPlugin(new TemplatePlugin());
+      web3.registerPlugin(new IpfsPlugin());
       consoleSpy = jest.spyOn(global.console, "log").mockImplementation();
     });
 
@@ -23,8 +23,8 @@ describe("TemplatePlugin Tests", () => {
       consoleSpy.mockRestore();
     });
 
-    it("should call TempltyPlugin test method with expected param", () => {
-      web3.template.test("test-param");
+    it("should call IpfsPlugin test method with expected param", () => {
+      web3.ipfs.test("test-param");
       expect(consoleSpy).toHaveBeenCalledWith("test-param");
     });
   });
